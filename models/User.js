f379d257f96221563dcb01d06465d48b0ac5aa17
ademlsgo <145021201+ademlsgo@users.noreply.github.com>
2025-11-14 01:45:23 +0100

    Fix User model mapping (underscored + google_id)

--- models/User.js
+++ models/User.js
@@ -2,78 +2,44 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const bcrypt = require('bcryptjs');
 
-const user = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  first_name: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      len: [2, 50]
-    }
-  },
-  last_name: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      len: [2, 50]
-    }
-  },
-  identifiant: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-    unique: true,
-    validate: {
-      len: [3, 50]
-    }
-  },
-  email: {
-    type: DataTypes.STRING(100),
-    allowNull: true,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    validate: {
-      len: [8, 100]
-    }
-  },
-  googleId: {
-    type: DataTypes.STRING(255),
-    allowNull: true,
-    unique: true
-  },
-  role: {
-    type: DataTypes.ENUM('user', 'superuser', 'employee'),
-    defaultValue: 'user',
-    allowNull: false
-  }
-}, {
-  tableName: 'users',
-  timestamps: true,
-  hooks: {
-    beforeCreate: async (user) => {
-      // Ne hasher le mot de passe que s'il est fourni (pas pour les utilisateurs Google)
-      if (user.password && !user.googleId) {
-        user.password = await bcrypt.hash(user.password, 10);
-      }
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
-    beforeUpdate: async (user) => {
-      // Ne hasher le mot de passe que s'il est fourni et modifié
-      if (user.changed('password') && user.password && !user.googleId) {
-        user.password = await bcrypt.hash(user.password, 10);
-      }
+    first_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    identifiant: {
+        type: DataTypes.STRING(50),
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    googleId: {
+        field: 'google_id',   // IMPORTANT pour mapper google_id
+        type: DataTypes.STRING(255),
+        unique: true
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'superuser', 'employee'),
+        defaultValue: 'user'
     }
-  }
+}, {
+    tableName: 'users',
+    underscored: true,     // <-- LA LIGNE QUI FIXE TOUT
+    timestamps: true
 });
 
-module.exports = user;
+module.exports = User;
